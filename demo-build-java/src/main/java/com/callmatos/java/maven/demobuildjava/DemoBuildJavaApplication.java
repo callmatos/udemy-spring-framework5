46c69cb1ebd3c253888c72fe1b933f7c799bbf88
Charles Araujo <callmatos@gmail.com>
2023-12-23 08:24:12 -0400

    adding commandLine to show all beans provide by Spring

--- demo-build-java/src/main/java/com/callmatos/java/maven/demobuildjava/DemoBuildJavaApplication.java
+++ demo-build-java/src/main/java/com/callmatos/java/maven/demobuildjava/DemoBuildJavaApplication.java
@@ -15,16 +15,16 @@ public class DemoBuildJavaApplication {
 		SpringApplication.run(DemoBuildJavaApplication.class, args);
 	}
 
-	// @Bean
-	// public CommandLineRunner commandLineRunner(ApplicationContext ctx){
-	// 	return args -> {
-	// 		System.out.println("Let's inspect the beans provide by Spring boot");
-	// 		String[] beanNames = ctx.getBeanDefinitionNames();
-	// 		Arrays.sort(beanNames);
-	// 		for (String beanName : beanNames){
-	// 			System.out.println(beanName);
-	// 		}
-	// 	};
-	// }
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("Let's inspect the beans provide by Spring boot");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames){
+				System.out.println(beanName);
+			}
+		};
+	}
 
 }
